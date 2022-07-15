67845a6233ebb150836c4f1444565af26c3e1cb7
Sharkri <sharkdude955@gmail.com>
2022-07-15 04:29:49 -0500

    added image background and other stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import home from "./pages/home";
 import contact from "./pages/contact";
 import menu from "./pages/menu";
+
 const content = document.querySelector("#content");
 
 const homepageBtn = document.createElement("button");
@@ -10,15 +11,27 @@ homepageBtn.textContent = "Home";
 menuBtn.textContent = "Menu";
 contactBtn.textContent = "Contact";
 const buttons = document.createElement("div");
+buttons.classList.add("buttons");
+const img = new Image();
+img.src = `https://images.unsplash.com/photo-1541709521-b9bae54e4aae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80`;
 buttons.appendChild(homepageBtn);
 buttons.appendChild(menuBtn);
 buttons.appendChild(contactBtn);
-buttons.classList.add("buttons");
-content.appendChild(buttons);
+
+const header = document.createElement("div");
+header.classList.add("header");
+const headerText = document.createElement("h1");
+headerText.textContent = "generic restaurant";
+
+header.appendChild(headerText);
+header.appendChild(buttons);
+
 const main = document.createElement("div");
 main.classList.add("main");
-content.appendChild(main);
 
+content.appendChild(header);
+content.appendChild(main);
+content.appendChild(img);
 // Default Page
 switchTab(home());
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,8 @@
 export default function home() {
-  const header = document.createElement("h1");
   const para = document.createElement("p");
   const paraDiv = document.createElement("div");
   const container = document.createElement("div");
-
-  header.textContent = "Generic restaurant ™";
   para.textContent = "Generic restaurant ™ has only the best foods";
-  container.appendChild(header);
   paraDiv.appendChild(para);
   container.appendChild(paraDiv);
   return container;
